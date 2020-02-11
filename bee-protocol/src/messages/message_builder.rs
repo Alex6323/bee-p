34b1908c4abf64b780d521d91b61dc731df17e8e
Thibault Martinez <thibault@iota.org>
2020-02-11 16:33:11 +0100

    MessageType

--- bee-protocol/src/messages/message_builder.rs
+++ bee-protocol/src/messages/message_builder.rs
@@ -1,13 +1,31 @@
+use crate::messages::errors::MessageError;
 use crate::messages::handshake::Handshake;
 use crate::messages::header::Header;
-use crate::messages::Message;
-use crate::messages::MessageType;
+use crate::messages::heartbeat::Heartbeat;
+use crate::messages::legacy_gossip::LegacyGossip;
+use crate::messages::milestone_request::MilestoneRequest;
+use crate::messages::transaction_broadcast::TransactionBroadcast;
+use crate::messages::transaction_request::TransactionRequest;
+use crate::messages::{Message, MessageType};
 
-pub fn create_message(bytes: &[u8]) -> impl Message {
+pub fn create_message(bytes: &[u8]) -> Result<MessageType, MessageError> {
     let header = Header::from_bytes(&bytes[Header::size_range()]);
 
     match bytes[0] {
-        0 => Handshake::new(),
-        _ => unreachable!(),
+        0x01 => Ok(MessageType::Handshake(Handshake::from_bytes(&bytes[3..])?)),
+        0x02 => Ok(MessageType::LegacyGossip(LegacyGossip::from_bytes(
+            &bytes[3..],
+        )?)),
+        0x03 => Ok(MessageType::MilestoneRequest(MilestoneRequest::from_bytes(
+            &bytes[3..],
+        )?)),
+        0x04 => Ok(MessageType::TransactionBroadcast(
+            TransactionBroadcast::from_bytes(&bytes[3..])?,
+        )),
+        0x05 => Ok(MessageType::TransactionRequest(
+            TransactionRequest::from_bytes(&bytes[3..])?,
+        )),
+        0x06 => Ok(MessageType::Heartbeat(Heartbeat::from_bytes(&bytes[3..])?)),
+        _ => Err(MessageError::UnknownMessageType(bytes[0])),
     }
 }
--- bee-protocol/src/messages/mod.rs
+++ bee-protocol/src/messages/mod.rs
@@ -19,31 +19,12 @@ pub use milestone_request::MilestoneRequest;
 pub use transaction_broadcast::TransactionBroadcast;
 pub use transaction_request::TransactionRequest;
 
-use std::convert::TryFrom;
-
 pub enum MessageType {
-    Header,
-    Handshake,
-    LegacyGossip,
-    MilestoneRequest,
-    TransactionBroadcast,
-    TransactionRequest,
-    Heartbeat,
-}
-
-impl TryFrom<u8> for MessageType {
-    type Error = MessageError;
-
-    fn try_from(byte: u8) -> Result<Self, Self::Error> {
-        return match byte {
-            0x00 => Ok(MessageType::Header),
-            0x01 => Ok(MessageType::Handshake),
-            0x02 => Ok(MessageType::LegacyGossip),
-            0x03 => Ok(MessageType::MilestoneRequest),
-            0x04 => Ok(MessageType::TransactionBroadcast),
-            0x05 => Ok(MessageType::TransactionRequest),
-            0x06 => Ok(MessageType::Heartbeat),
-            _ => Err(MessageError::UnknownMessageType(byte)),
-        };
-    }
+    Header(Header),
+    Handshake(Handshake),
+    LegacyGossip(LegacyGossip),
+    MilestoneRequest(MilestoneRequest),
+    TransactionBroadcast(TransactionBroadcast),
+    TransactionRequest(TransactionRequest),
+    Heartbeat(Heartbeat),
 }
