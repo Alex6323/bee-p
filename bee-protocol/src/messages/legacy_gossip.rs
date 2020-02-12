5c8557b3cb5fee1552fded79b7245379b4becad5
Thibault Martinez <thibault@iota.org>
2020-02-12 16:23:41 +0100

    LegacyGossip impl

--- bee-protocol/src/messages/legacy_gossip.rs
+++ bee-protocol/src/messages/legacy_gossip.rs
@@ -3,19 +3,21 @@ use crate::messages::message::Message;
 
 use std::ops::Range;
 
-const LEGACY_GOSSIP_CONSTANT_SIZE: usize = 49;
+const LEGACY_GOSSIP_REQUEST_SIZE: usize = 49;
+const LEGACY_GOSSIP_CONSTANT_SIZE: usize = LEGACY_GOSSIP_REQUEST_SIZE;
 const LEGACY_GOSSIP_VARIABLE_MIN_SIZE: usize = 292;
 const LEGACY_GOSSIP_VARIABLE_MAX_SIZE: usize = 1604;
 
 pub struct LegacyGossip {
     transaction: Vec<u8>,
-    request: [u8; LEGACY_GOSSIP_CONSTANT_SIZE],
+    request: [u8; LEGACY_GOSSIP_REQUEST_SIZE],
 }
 
 impl LegacyGossip {
-    pub fn new(transaction: Vec<u8>, request: [u8; LEGACY_GOSSIP_CONSTANT_SIZE]) -> Self {
+    pub fn new(transaction: &Vec<u8>, request: [u8; LEGACY_GOSSIP_REQUEST_SIZE]) -> Self {
+        // TODO clone ?
         Self {
-            transaction: transaction,
+            transaction: transaction.clone(),
             request: request,
         }
     }
@@ -24,7 +26,7 @@ impl LegacyGossip {
         &self.transaction
     }
 
-    pub fn request(&self) -> &[u8; LEGACY_GOSSIP_CONSTANT_SIZE] {
+    pub fn request(&self) -> &[u8; LEGACY_GOSSIP_REQUEST_SIZE] {
         &self.request
     }
 }
@@ -40,14 +42,28 @@ impl Message for LegacyGossip {
             Err(MessageError::InvalidMessageLength(bytes.len()))?;
         }
 
+        let mut offset = 0;
+
+        let mut transaction = Vec::new();
+        transaction
+            .extend_from_slice(&bytes[offset..offset + bytes.len() - LEGACY_GOSSIP_REQUEST_SIZE]);
+        offset += bytes.len() - LEGACY_GOSSIP_REQUEST_SIZE;
+
+        let mut request = [0; LEGACY_GOSSIP_REQUEST_SIZE];
+        request.copy_from_slice(&bytes[offset..offset + LEGACY_GOSSIP_REQUEST_SIZE]);
+
         Ok(Self {
-            transaction: Vec::new(),
-            request: [0; LEGACY_GOSSIP_CONSTANT_SIZE],
+            transaction: transaction,
+            request: request,
         })
     }
 
     fn into_bytes(self) -> Vec<u8> {
-        [].to_vec()
+        let mut bytes = self.transaction.clone();
+
+        bytes.extend_from_slice(&self.request);
+
+        bytes
     }
 }
 
@@ -55,6 +71,21 @@ impl Message for LegacyGossip {
 mod tests {
 
     use super::*;
+
+    // TODO Move to utils ?
+    fn eq<'a, T: PartialEq>(a: &'a [T], b: &'a [T]) -> bool {
+        if a.len() != b.len() {
+            return false;
+        }
+
+        for i in 0..a.len() {
+            if a[i] != b[i] {
+                return false;
+            }
+        }
+
+        true
+    }
 
     #[test]
     fn size_range_test() {
@@ -77,5 +108,20 @@ mod tests {
             Err(MessageError::InvalidMessageLength(length)) => assert_eq!(length, 1654),
             _ => unreachable!(),
         }
+    }
+
+    #[test]
+    fn new_into_from_test() {
+        let transaction: Vec<u8> = (500..1000).map(|i| i as u8).collect();
+        let request = [
+            160, 3, 36, 228, 202, 18, 56, 37, 229, 28, 240, 65, 225, 238, 64, 55, 244, 83, 155,
+            232, 31, 255, 208, 9, 126, 21, 82, 57, 180, 237, 182, 101, 242, 57, 202, 28, 118, 203,
+            67, 93, 74, 238, 57, 39, 51, 169, 193, 124, 254,
+        ];
+        let message_from = LegacyGossip::new(&transaction, request);
+        let message_to = LegacyGossip::from_bytes(&message_from.into_bytes()).unwrap();
+
+        assert_eq!(message_to.transaction(), &transaction);
+        assert_eq!(eq(message_to.request(), &request), true);
     }
 }
