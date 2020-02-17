4343fcf197bbed1a437075cca4bf55e378119d47
Thibault Martinez <thibault@iota.org>
2020-02-17 14:18:32 +0100

    MessageType file

---
+++ bee-protocol/src/messages/message_type.rs
@@ -0,0 +1,34 @@
+use crate::messages::handshake::Handshake;
+use crate::messages::heartbeat::Heartbeat;
+use crate::messages::legacy_gossip::LegacyGossip;
+use crate::messages::message::Message;
+use crate::messages::milestone_request::MilestoneRequest;
+use crate::messages::transaction_broadcast::TransactionBroadcast;
+use crate::messages::transaction_request::TransactionRequest;
+
+use std::ops::Deref;
+
+#[non_exhaustive]
+pub enum MessageType {
+    Handshake(Handshake),
+    LegacyGossip(LegacyGossip),
+    MilestoneRequest(MilestoneRequest),
+    TransactionBroadcast(TransactionBroadcast),
+    TransactionRequest(TransactionRequest),
+    Heartbeat(Heartbeat),
+}
+
+impl Deref for MessageType {
+    type Target = dyn Message;
+
+    fn deref<'a>(&'a self) -> &'a Self::Target {
+        match self {
+            MessageType::Handshake(message) => message,
+            MessageType::LegacyGossip(message) => message,
+            MessageType::MilestoneRequest(message) => message,
+            MessageType::TransactionBroadcast(message) => message,
+            MessageType::TransactionRequest(message) => message,
+            MessageType::Heartbeat(message) => message,
+        }
+    }
+}
--- bee-protocol/src/messages/mod.rs
+++ bee-protocol/src/messages/mod.rs
@@ -4,6 +4,7 @@ mod heartbeat;
 mod legacy_gossip;
 mod message;
 mod message_builder;
+mod message_type;
 mod milestone_request;
 mod transaction_broadcast;
 mod transaction_request;
@@ -14,33 +15,7 @@ pub use heartbeat::Heartbeat;
 pub use legacy_gossip::LegacyGossip;
 pub use message::Message;
 pub use message_builder::read_message;
+pub use message_type::MessageType;
 pub use milestone_request::MilestoneRequest;
 pub use transaction_broadcast::TransactionBroadcast;
 pub use transaction_request::TransactionRequest;
-
-use std::ops::Deref;
-
-#[non_exhaustive]
-pub enum MessageType {
-    Handshake(Handshake),
-    LegacyGossip(LegacyGossip),
-    MilestoneRequest(MilestoneRequest),
-    TransactionBroadcast(TransactionBroadcast),
-    TransactionRequest(TransactionRequest),
-    Heartbeat(Heartbeat),
-}
-
-impl Deref for MessageType {
-    type Target = dyn Message;
-
-    fn deref<'a>(&'a self) -> &'a Self::Target {
-        match self {
-            MessageType::Handshake(message) => message,
-            MessageType::LegacyGossip(message) => message,
-            MessageType::MilestoneRequest(message) => message,
-            MessageType::TransactionBroadcast(message) => message,
-            MessageType::TransactionRequest(message) => message,
-            MessageType::Heartbeat(message) => message,
-        }
-    }
-}
