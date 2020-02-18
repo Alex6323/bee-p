caf317095799973a617848169787138d4802781a
Thibault Martinez <thibault@iota.org>
2020-02-18 10:20:07 +0100

    Remove old Messages from network

--- bee-network/src/read_task_broker.rs
+++ bee-network/src/read_task_broker.rs
@@ -1,55 +1,59 @@
 use std::{
-    sync::Arc,
-    io::{Error, ErrorKind},
     collections::HashMap,
-    convert::TryFrom
+    convert::TryFrom,
+    io::{Error, ErrorKind},
+    sync::Arc,
 };
 
-use futures::{channel::mpsc, select, FutureExt, SinkExt, lock::Mutex};
+use futures::{channel::mpsc, lock::Mutex, select, FutureExt, SinkExt};
 
 use async_std::{
     io::BufReader,
-    net::{TcpStream, SocketAddr},
+    net::{SocketAddr, TcpStream},
     prelude::*,
-    task
+    task,
 };
 
-use crate::message::Message;
-use crate::message::MessageType;
 use crate::message::ReceivedMessage;
+use bee_protocol::{Message, MessageType};
 
 type Sender<T> = mpsc::UnboundedSender<T>;
 type Receiver<T> = mpsc::UnboundedReceiver<T>;
 
-pub async fn read_task_broker(mut read_task_receiver: Receiver<Arc<TcpStream>>, received_messages_sender: Sender<ReceivedMessage>, shutdown_handles_of_read_tasks: Arc<Mutex<HashMap<SocketAddr, Sender<()>>>>) {
-
+pub async fn read_task_broker(
+    mut read_task_receiver: Receiver<Arc<TcpStream>>,
+    received_messages_sender: Sender<ReceivedMessage>,
+    shutdown_handles_of_read_tasks: Arc<Mutex<HashMap<SocketAddr, Sender<()>>>>,
+) {
     while let Some(stream) = read_task_receiver.next().await {
-
         match stream.peer_addr() {
-
             Ok(address) => {
                 let (read_task_shutdown_sender, read_task_shutdown_receiver) = mpsc::unbounded();
-                let shutdown_handles_of_read_tasks: &mut HashMap<SocketAddr, Sender<()>> = &mut *shutdown_handles_of_read_tasks.lock().await;
+                let shutdown_handles_of_read_tasks: &mut HashMap<SocketAddr, Sender<()>> =
+                    &mut *shutdown_handles_of_read_tasks.lock().await;
                 shutdown_handles_of_read_tasks.insert(address, read_task_shutdown_sender);
-                spawn_and_log_error(read_task(read_task_shutdown_receiver, stream, received_messages_sender.clone()));
-            },
+                spawn_and_log_error(read_task(
+                    read_task_shutdown_receiver,
+                    stream,
+                    received_messages_sender.clone(),
+                ));
+            }
 
             Err(e) => {
                 eprintln!("{}", e);
             }
-
         }
-
     }
-
 }
 
-async fn read_task(mut shutdown_task: Receiver<()>, stream: Arc<TcpStream>, mut received_messages: Sender<ReceivedMessage>) -> Result<(), Error> {
-
+async fn read_task(
+    mut shutdown_task: Receiver<()>,
+    stream: Arc<TcpStream>,
+    mut received_messages: Sender<ReceivedMessage>,
+) -> Result<(), Error> {
     let mut reader = BufReader::new(&*stream);
 
     loop {
-
         // 1) Check message type
         let mut message_type_buf = [0u8; 1];
         select! {
@@ -67,37 +71,43 @@ async fn read_task(mut shutdown_task: Receiver<()>, stream: Arc<TcpStream>, mut 
         let message_length_as_usize = usize::try_from(u16::from_be_bytes(message_length_buf));
 
         if let Err(_) = message_length_as_usize {
-            return Err(Error::new(ErrorKind::InvalidInput, "Can't convert message_length to usize"));
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "Can't convert message_length to usize",
+            ));
         }
 
         let message_length = message_length_as_usize.unwrap();
 
         // 3) Parse message based on type and length
-        match message_type {
-
-            1 => {
-
-                let mut test_message_buf = vec![0u8; message_length];
-                select! {
-                    result = reader.read_exact(&mut test_message_buf).fuse() => result?,
-                    void = shutdown_task.next().fuse() => break
-                }
-
-                received_messages.send(ReceivedMessage{from: stream.peer_addr()?, msg: MessageType::Test(Message::new(test_message_buf)? ) }).await.unwrap();
-
-            },
-
-            _ => return Err(Error::new(ErrorKind::InvalidInput, "Invalid message type"))
-
-        }
-
+        // match message_type {
+        //     1 => {
+        //         let mut test_message_buf = vec![0u8; message_length];
+        //         select! {
+        //             result = reader.read_exact(&mut test_message_buf).fuse() => result?,
+        //             void = shutdown_task.next().fuse() => break
+        //         }
+        //
+        //         received_messages
+        //             .send(ReceivedMessage {
+        //                 from: stream.peer_addr()?,
+        //                 msg: MessageType::Test(Message::new(test_message_buf)?),
+        //             })
+        //             .await
+        //             .unwrap();
+        //     }
+        //
+        //     _ => return Err(Error::new(ErrorKind::InvalidInput, "Invalid message type")),
+        // }
     }
 
     Ok(())
-
 }
 
-fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()> where F: Future<Output = Result<(), Error>> + Send + 'static {
+fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
+where
+    F: Future<Output = Result<(), Error>> + Send + 'static,
+{
     task::spawn(async move {
         if let Err(e) = fut.await {
             eprintln!("{}", e)
--- bee-network/src/write_task_broker.rs
+++ bee-network/src/write_task_broker.rs
@@ -1,65 +1,69 @@
 use std::{
-    sync::Arc,
-    io::{Error, ErrorKind},
     collections::HashMap,
     convert::TryInto,
+    io::{Error, ErrorKind},
+    sync::Arc,
 };
 
-use futures::{channel::mpsc, select, FutureExt, SinkExt, lock::Mutex};
+use futures::{channel::mpsc, lock::Mutex, select, FutureExt, SinkExt};
 
 use async_std::{
-    net::{TcpStream, SocketAddr},
+    net::{SocketAddr, TcpStream},
     prelude::*,
-    task
+    task,
 };
 
-use crate::message::Message;
-use crate::message::MessageType;
 use crate::message::MessageToSend;
+use bee_protocol::{Message, MessageType};
 
 type Sender<T> = mpsc::UnboundedSender<T>;
 type Receiver<T> = mpsc::UnboundedReceiver<T>;
 
-pub async fn write_task_broker(mut write_task_receiver: Receiver<Arc<TcpStream>>, senders_of_write_tasks: Arc<Mutex<HashMap<SocketAddr, Sender<MessageToSend>>>>, shutdown_handles_of_write_tasks: Arc<Mutex<HashMap<SocketAddr, Sender<()>>>>, mut connected_peers_sender: Sender<SocketAddr>) {
-
+pub async fn write_task_broker(
+    mut write_task_receiver: Receiver<Arc<TcpStream>>,
+    senders_of_write_tasks: Arc<Mutex<HashMap<SocketAddr, Sender<MessageToSend>>>>,
+    shutdown_handles_of_write_tasks: Arc<Mutex<HashMap<SocketAddr, Sender<()>>>>,
+    mut connected_peers_sender: Sender<SocketAddr>,
+) {
     while let Some(stream) = write_task_receiver.next().await {
-
         match stream.peer_addr() {
-
             Ok(address) => {
-
                 // register shutdown_sender of individual write_task
                 let (write_task_shutdown_sender, write_task_shutdown_receiver) = mpsc::unbounded();
-                let shutdown_handles_of_write_tasks: &mut HashMap<SocketAddr, Sender<()>> = &mut *shutdown_handles_of_write_tasks.lock().await;
+                let shutdown_handles_of_write_tasks: &mut HashMap<SocketAddr, Sender<()>> =
+                    &mut *shutdown_handles_of_write_tasks.lock().await;
                 shutdown_handles_of_write_tasks.insert(address.clone(), write_task_shutdown_sender);
 
                 // register message_sender of individual write_task
                 let (write_task_message_sender, write_task_message_receiver) = mpsc::unbounded();
-                let senders_of_write_tasks: &mut HashMap<SocketAddr, Sender<MessageToSend>> = &mut *senders_of_write_tasks.lock().await;
+                let senders_of_write_tasks: &mut HashMap<SocketAddr, Sender<MessageToSend>> =
+                    &mut *senders_of_write_tasks.lock().await;
                 senders_of_write_tasks.insert(address.clone(), write_task_message_sender);
 
                 connected_peers_sender.send(address).await.unwrap();
 
-                spawn_and_log_error(write_task(write_task_shutdown_receiver, stream, write_task_message_receiver));
-
-            },
+                spawn_and_log_error(write_task(
+                    write_task_shutdown_receiver,
+                    stream,
+                    write_task_message_receiver,
+                ));
+            }
 
             Err(e) => {
                 eprintln!("{}", e);
             }
-
         }
-
     }
-
 }
 
-async fn write_task(mut shutdown_task: Receiver<()>, stream: Arc<TcpStream>, mut message_receiver: Receiver<MessageToSend>) -> Result<(), Error> {
-
+async fn write_task(
+    mut shutdown_task: Receiver<()>,
+    stream: Arc<TcpStream>,
+    mut message_receiver: Receiver<MessageToSend>,
+) -> Result<(), Error> {
     let mut stream = &*stream;
 
     loop {
-
         let message = select! {
 
             message = message_receiver.next().fuse() => match message {
@@ -77,36 +81,34 @@ async fn write_task(mut shutdown_task: Receiver<()>, stream: Arc<TcpStream>, mut
             continue;
         }
 
-        match message.msg {
-
-            MessageType::Test(msg) => {
-
-                let message_type = [1u8;1];
-                let message_length;
-
-                let message_length_result: Result<u16, std::num::TryFromIntError> = msg.bytes().len().try_into();
-
-                if let Err(_) = message_length_result {
-                    return Err(Error::new(ErrorKind::InvalidInput, "Message is too big"));
-                }
-
-                message_length = message_length_result.unwrap();
-
-                stream.write_all(&message_type).await?;
-                stream.write_all(&message_length.to_be_bytes()).await?;
-                stream.write_all(msg.bytes()).await?;
-
-            }
-
-        }
-
+        // match message.msg {
+        //     MessageType::Test(msg) => {
+        //         let message_type = [1u8; 1];
+        //         let message_length;
+        //
+        //         let message_length_result: Result<u16, std::num::TryFromIntError> =
+        //             msg.bytes().len().try_into();
+        //
+        //         if let Err(_) = message_length_result {
+        //             return Err(Error::new(ErrorKind::InvalidInput, "Message is too big"));
+        //         }
+        //
+        //         message_length = message_length_result.unwrap();
+        //
+        //         stream.write_all(&message_type).await?;
+        //         stream.write_all(&message_length.to_be_bytes()).await?;
+        //         stream.write_all(msg.bytes()).await?;
+        //     }
+        // }
     }
 
     Ok(())
-
 }
 
-fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()> where F: Future<Output = Result<(), Error>> + Send + 'static {
+fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
+where
+    F: Future<Output = Result<(), Error>> + Send + 'static,
+{
     task::spawn(async move {
         if let Err(e) = fut.await {
             eprintln!("{}", e)
