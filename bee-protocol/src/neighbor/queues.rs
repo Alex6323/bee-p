609cb4080bc643ed0f6bb271336b454f3a04d51a
Thibault Martinez <thibault@iota.org>
2020-02-25 14:54:06 +0100

    Visibility of queues

--- bee-protocol/src/neighbor/queues.rs
+++ bee-protocol/src/neighbor/queues.rs
@@ -1,11 +1,10 @@
-use crate::message::Heartbeat;
-use crate::message::LegacyGossip;
-use crate::message::MilestoneRequest;
-use crate::message::TransactionBroadcast;
-use crate::message::TransactionRequest;
+use crate::message::{
+    Handshake, Heartbeat, LegacyGossip, MilestoneRequest, TransactionBroadcast, TransactionRequest,
+};
 
 use futures::channel::mpsc::{channel, Receiver, Sender};
 
+const HANDSHAKE_QUEUE_SIZE: usize = 1000;
 const HEARTBEAT_QUEUE_SIZE: usize = 1000;
 const LEGACY_GOSSIP_QUEUE_SIZE: usize = 1000;
 const MILESTONE_REQUEST_QUEUE_SIZE: usize = 1000;
@@ -13,16 +12,19 @@ const TRANSACTION_BROADCAST_QUEUE_SIZE: usize = 1000;
 const TRANSACTION_REQUEST_QUEUE_SIZE: usize = 1000;
 
 pub(crate) struct NeighborQueues {
-    heartbeat: (Sender<Heartbeat>, Receiver<Heartbeat>),
-    legacy_gossip: (Sender<LegacyGossip>, Receiver<LegacyGossip>),
-    milestone_request: (Sender<MilestoneRequest>, Receiver<MilestoneRequest>),
-    transaction_broadcast: (Sender<TransactionBroadcast>, Receiver<TransactionBroadcast>),
-    transaction_request: (Sender<TransactionRequest>, Receiver<TransactionRequest>),
+    pub(crate) handshake: (Sender<Handshake>, Receiver<Handshake>),
+    pub(crate) heartbeat: (Sender<Heartbeat>, Receiver<Heartbeat>),
+    pub(crate) legacy_gossip: (Sender<LegacyGossip>, Receiver<LegacyGossip>),
+    pub(crate) milestone_request: (Sender<MilestoneRequest>, Receiver<MilestoneRequest>),
+    pub(crate) transaction_broadcast:
+        (Sender<TransactionBroadcast>, Receiver<TransactionBroadcast>),
+    pub(crate) transaction_request: (Sender<TransactionRequest>, Receiver<TransactionRequest>),
 }
 
 impl Default for NeighborQueues {
     fn default() -> Self {
         Self {
+            handshake: channel(HANDSHAKE_QUEUE_SIZE),
             heartbeat: channel(HEARTBEAT_QUEUE_SIZE),
             legacy_gossip: channel(LEGACY_GOSSIP_QUEUE_SIZE),
             milestone_request: channel(MILESTONE_REQUEST_QUEUE_SIZE),
