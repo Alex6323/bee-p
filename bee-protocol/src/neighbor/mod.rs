2f282b68c04e098ee3b989c4ef8e894aa62ed6e8
Thibault Martinez <thibault@iota.org>
2020-03-04 09:36:59 +0100

    Channels -> Channel

--- bee-protocol/src/neighbor/mod.rs
+++ bee-protocol/src/neighbor/mod.rs
@@ -1,9 +1,9 @@
-mod channels;
+mod channel;
 mod event;
 mod neighbor;
 mod receiver_actor;
 
-pub(crate) use channels::{NeighborChannels, NeighborSenders};
+pub(crate) use channel::{NeighborChannels, NeighborSenders};
 pub(crate) use event::NeighborEvent;
 pub(crate) use neighbor::Neighbor;
 pub(crate) use receiver_actor::NeighborReceiverActor;
