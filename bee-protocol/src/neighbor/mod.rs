3d710c014770766e30842012d6e1b517f5c853c1
Thibault Martinez <thibault@iota.org>
2020-03-04 09:35:09 +0100

    Receiver actor module

--- bee-protocol/src/neighbor/mod.rs
+++ bee-protocol/src/neighbor/mod.rs
@@ -1,5 +1,7 @@
 mod channels;
 mod neighbor;
+mod receiver_actor;
 
 pub(crate) use channels::{NeighborChannels, NeighborSenders};
-pub(crate) use neighbor::{Neighbor, NeighborEvent, NeighborReceiverActor};
+pub(crate) use neighbor::{Neighbor, NeighborEvent};
+pub(crate) use receiver_actor::NeighborReceiverActor;
