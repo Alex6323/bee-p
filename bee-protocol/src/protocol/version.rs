bd8e93507346f95f84fd05c88aeb2f9ae5643e0b
Thibault Martinez <thibault@iota.org>
2020-03-17 14:14:14 +0100

    Remove version legacy

--- bee-protocol/src/protocol/version.rs
+++ bee-protocol/src/protocol/version.rs
@@ -1,6 +1,5 @@
-const VERSION_LEGACY: u8 = 1 << 0;
 const VERSION_STING: u8 = 1 << 1;
-pub(crate) const SUPPORTED_VERSIONS: [u8; 1] = [VERSION_LEGACY | VERSION_STING];
+pub(crate) const SUPPORTED_VERSIONS: [u8; 1] = [VERSION_STING];
 
 enum SupportedVersion {
     CommonSupportedVersion(u8),
