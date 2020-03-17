e7f02a48bfa9de60010848d7878276e8f95ac4a3
Tsvi <tsvi.sabo@iota.org>
2020-03-17 10:47:19 +0200

    bee-bundle: validation - add more checks

--- bee-bundle/src/transaction.rs
+++ bee-bundle/src/transaction.rs
@@ -7,7 +7,7 @@ use std::fmt;
 use std::iter;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct Payload(TritBuf<T1B1Buf>);
+pub struct Payload(pub TritBuf<T1B1Buf>);
 
 impl Payload {
     pub fn zeros() -> Self {
@@ -31,7 +31,7 @@ impl Payload {
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct Address(TritBuf<T1B1Buf>);
+pub struct Address(pub TritBuf<T1B1Buf>);
 
 impl Address {
     pub fn zeros() -> Self {
@@ -58,7 +58,7 @@ impl Address {
 pub struct Value(pub i64);
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct Tag(TritBuf<T1B1Buf>);
+pub struct Tag(pub TritBuf<T1B1Buf>);
 
 impl Tag {
     pub fn zeros() -> Self {
@@ -88,7 +88,7 @@ pub struct Timestamp(pub u64);
 pub struct Index(pub usize);
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct Hash(TritBuf<T1B1Buf>);
+pub struct Hash(pub TritBuf<T1B1Buf>);
 
 impl Hash {
     pub fn zeros() -> Self {
@@ -112,7 +112,7 @@ impl Hash {
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct Nonce(TritBuf<T1B1Buf>);
+pub struct Nonce(pub TritBuf<T1B1Buf>);
 
 impl Nonce {
     pub fn zeros() -> Self {
