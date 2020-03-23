67e897700d977a6eea4078b28a260f1bb8b7f248
Thibault Martinez <thibault@iota.org>
2020-03-23 09:30:10 +0100

    Remove comments

--- bee-node/src/main.rs
+++ bee-node/src/main.rs
@@ -16,18 +16,5 @@ fn main() {
     let mut node = Node::new(network, shutdown, receiver);
 
     block_on(node.init());
-
     block_on(node.run());
-
-    // task::spawn(notification_handler(receiver));
-    //
-    // block_on(node.init());
-    //
-    // // NOTE: all the node business logic has to go inside of the following scope!!!
-    // {
-    //     // For example: spamming the network
-    //     std::thread::spawn(|| spam(network, msg, 50, 1000));
-    // }
-    //
-    // block_on(node.shutdown());
 }
