1d7d5084fa0917a68221f9c054f145793bef9331
Thibault Martinez <thibault@iota.org>
2020-03-23 09:38:20 +0100

    Welcome message

--- bee-node/src/main.rs
+++ bee-node/src/main.rs
@@ -1,14 +1,23 @@
+mod constants;
 mod logger;
 mod node;
 
+use constants::{
+    BEE_NAME,
+    BEE_VERSION,
+};
 use node::Node;
 
 use bee_network::Address;
 
 use async_std::task::block_on;
+use log::info;
 
 fn main() {
+    // TODO conf variable
     logger::init(log::LevelFilter::Debug);
+
+    info!("[Main ] Welcome to {} {}!", BEE_NAME, BEE_VERSION);
 
     let addr = block_on(Address::from_host_addr("localhost:1337")).unwrap();
     let (network, shutdown, receiver) = bee_network::init(addr);
