c993911753386270927e4cd6d7d28f4736958440
Thibault Martinez <thibault@iota.org>
2020-04-02 09:51:53 +0200

    Remove unused enum variant

--- bee-protocol/src/message/errors.rs
+++ bee-protocol/src/message/errors.rs
@@ -1,6 +1,5 @@
 #[derive(Debug)]
 pub(crate) enum MessageError {
-    InvalidAdvertisedLengthBytes([u8; 2]),
     InvalidAdvertisedType(u8, u8),
     InvalidAdvertisedLength(usize, usize),
     InvalidPayloadLength(usize),
