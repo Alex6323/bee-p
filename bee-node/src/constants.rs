9fe1b3d292e625aefe8d69a500cdbd133fa534a0
Thibault Martinez <thibault@iota.org>
2020-04-11 13:03:49 +0200

    Clean bee-node folder

--- bee-node/src/constants.rs
+++ bee-node/src/constants.rs
@@ -1,3 +1,3 @@
-pub const CONFIG_PATH: &str = "./config.json";
-pub const BEE_NAME: &str = "Bee";
-pub const BEE_VERSION: &str = "0.1.0";
+pub(crate) const CONFIG_PATH: &str = "./config.json";
+pub(crate) const BEE_NAME: &str = "Bee";
+pub(crate) const BEE_VERSION: &str = "0.1.0";
