c07db775922d1fb16ecb4c52c3e31abb7f3a90e7
Thibault Martinez <thibault@iota.org>
2020-06-03 12:07:47 +0200

    Move cleanup.sql file

--- bee-storage/src/backends/sqlx/schemes/postgres/teardown.sh
+++ bee-storage/src/backends/sqlx/schemes/postgres/teardown.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
 USER="$1"
 DB_NAME="$2"
-CLEANUP_FILE="cleanup.sql"
-
+CLEANUP_FILE="src/backends/sqlx/schemes/postgres/cleanup.sql"
 
 echo "DROP DATABASE "$DB_NAME";" > $CLEANUP_FILE
 
