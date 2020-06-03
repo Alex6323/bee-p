5beac7ecf3637079a824b033e0911d6af6a00bc3
Thibault Martinez <thibault@iota.org>
2020-06-03 12:21:23 +0200

    Rocksdb models module

--- bee-storage/src/backends/rocksdb/mod.rs
+++ bee-storage/src/backends/rocksdb/mod.rs
@@ -9,9 +9,8 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-extern crate rocksdb;
-
 mod errors;
+mod models;
 mod test;
 
 use crate::storage::{
--- bee-storage/src/backends/rocksdb/models/milestone.rs
+++ bee-storage/src/backends/rocksdb/models/milestone.rs
@@ -9,22 +9,26 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+use crate::{backends::rocksdb::RocksDbBackendError, models::MilestoneStorage};
+
 use bee_protocol::Milestone;
 use bee_transaction::Hash;
 
 use async_trait::async_trait;
 
-use std::{collections::HashSet, fmt::Debug};
+use std::collections::HashSet;
 
-#[async_trait]
-pub(crate) trait MilestoneStorage {
-    type Error: Debug;
+struct RocksDbMilestoneStorage;
 
-    async fn insert(&self, milestone: Milestone) -> Result<(), Self::Error>;
-
-    async fn get(&self, hash: Hash) -> Result<Milestone, Self::Error>;
-
-    async fn remove(&self, hash: Hash) -> Result<(), Self::Error>;
-
-    async fn remove_set(&self, hashes: &HashSet<Hash>) -> Result<(), Self::Error>;
-}
+// #[async_trait]
+// impl MilestoneStorage for RocksDbMilestoneStorage {
+//     type Error = RocksDbBackendError;
+//
+//     async fn insert(&self, milestone: Milestone) -> Result<(), Self::Error> {}
+//
+//     async fn get(&self, hash: Hash) -> Result<Milestone, Self::Error> {}
+//
+//     async fn remove(&self, hash: Hash) -> Result<(), Self::Error> {}
+//
+//     async fn remove_set(&self, hashes: &HashSet<Hash>) -> Result<(), Self::Error> {}
+// }
