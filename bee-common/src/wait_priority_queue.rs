70bd81a4ccbeabff6218a093ec858e52d3816976
Joshua Barretto <joshua.barretto@iota.org>
2020-06-16 14:11:36 +0100

    Fix WaitPriorityQueue (#141)
    * Added try_pop, pending, and incoming methods to WaitPriorityQueue
    
    * Removed unnecessary fuse calls
    
    * Fixed failed wakeup bug in WaitIncoming

--- bee-common/src/wait_priority_queue.rs
+++ bee-common/src/wait_priority_queue.rs
@@ -10,15 +10,25 @@
 // See the License for the specific language governing permissions and limitations under the License.
 
 use std::{
-    collections::{BinaryHeap, VecDeque},
+    collections::{
+        BinaryHeap,
+        VecDeque,
+    },
     future::Future,
     pin::Pin,
     sync::Mutex,
-    task::{Context, Poll, Waker},
+    task::{
+        Context,
+        Poll,
+        Waker,
+    },
+};
+use futures::{
+    future::FusedFuture,
+    stream::{Stream, FusedStream},
 };
 
 pub struct WaitPriorityQueue<T: Ord + Eq> {
-    // TODO use an RWLock ?
     inner: Mutex<(BinaryHeap<T>, VecDeque<Waker>)>,
 }
 
@@ -37,34 +47,93 @@ impl<T: Ord + Eq> Default for WaitPriorityQueue<T> {
 }
 
 impl<T: Ord + Eq> WaitPriorityQueue<T> {
-    pub fn push(&self, entry: T) {
+    /// Insert an item into the queue. It will be removed in an order consistent with the ordering
+    /// of itself relative to other items in the queue at the time of removal.
+    pub fn insert(&self, entry: T) {
         let mut inner = self.inner.lock().unwrap();
 
         inner.0.push(entry);
-        if let Some(waker) = inner.1.pop_front() {
-            Waker::wake(waker)
+        inner.1.pop_front().map(Waker::wake);
+    }
+
+    /// Attempt to remove the item with the highest priority from the queue, returning [`None`] if
+    /// there are no available items.
+    pub async fn try_pop(&self) -> Option<T> {
+        self.inner.lock().unwrap().0.pop()
+    }
+
+    /// Remove the item with the highest priority from the queue, waiting on an item should there
+    /// not be one immediately available.
+    pub fn pop(&self) -> impl Future<Output = T> + FusedFuture + '_ {
+        WaitFut {
+            queue: self,
+            terminated: false,
         }
     }
 
-    pub fn pop(&self) -> impl Future<Output = T> + '_ {
-        WaitFut(self)
+    /// Returns a stream of highest-priority items from this queue.
+    pub fn incoming(&self) -> impl Stream<Item = T> + FusedStream + '_ {
+        WaitIncoming {
+            queue: self,
+        }
+    }
+
+    /// Returns an iterator of pending items from this queue (i.e: those that are immediately
+    /// available).
+    pub fn pending(&self) -> impl Iterator<Item = T> + '_ {
+        std::iter::from_fn(move || self.inner.lock().unwrap().0.pop())
     }
 }
 
-pub struct WaitFut<'a, T: Ord + Eq>(&'a WaitPriorityQueue<T>);
+pub(crate) struct WaitFut<'a, T: Ord + Eq> {
+    queue: &'a WaitPriorityQueue<T>,
+    terminated: bool,
+}
 
 impl<'a, T: Ord + Eq> Future for WaitFut<'a, T> {
     type Output = T;
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        let mut inner = self.0.inner.lock().unwrap();
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        let mut inner = self.queue.inner.lock().unwrap();
 
         match inner.0.pop() {
-            Some(entry) => Poll::Ready(entry),
+            _ if self.terminated => Poll::Pending,
+            Some(entry) => {
+                self.terminated = true;
+                Poll::Ready(entry)
+            },
             None => {
                 inner.1.push_back(cx.waker().clone());
                 Poll::Pending
             }
         }
     }
+}
+
+impl<'a, T: Ord + Eq> FusedFuture for WaitFut<'a, T> {
+    fn is_terminated(&self) -> bool { self.terminated }
+}
+
+pub(crate) struct WaitIncoming<'a, T: Ord + Eq> {
+    queue: &'a WaitPriorityQueue<T>,
+}
+
+impl<'a, T: Ord + Eq> Stream for WaitIncoming<'a, T> {
+    type Item = T;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        let mut inner = self.queue.inner.lock().unwrap();
+
+        match inner.0.pop() {
+            Some(entry) => Poll::Ready(Some(entry)),
+            None => {
+                inner.1.push_back(cx.waker().clone());
+                Poll::Pending
+            },
+        }
+    }
+}
+
+impl<'a, T: Ord + Eq> FusedStream for WaitIncoming<'a, T> {
+    fn is_terminated(&self) -> bool { false }
 }
