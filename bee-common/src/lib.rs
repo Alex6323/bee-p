41cfaba531c7b7394260444fa1c17cb4b80c4d4b
Thibault Martinez <thibault@iota.org>
2020-06-25 10:40:46 +0200

    publicly export modules of bee-common

--- bee-common/src/lib.rs
+++ bee-common/src/lib.rs
@@ -9,11 +9,7 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+pub mod logger;
 pub mod shutdown;
+pub mod wait_priority_queue;
 pub mod worker;
-
-mod logger;
-mod wait_priority_queue;
-
-pub use logger::{logger_init, LoggerConfig, LoggerConfigBuilder};
-pub use wait_priority_queue::WaitPriorityQueue;
