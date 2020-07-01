7bb14d240eb7ae2da9f4080e8995bca8ab51bba0
Lucas Nogueira <lucas@tauri.studio>
2020-07-01 15:02:17 -0300

    chore(transaction) run fmt and add license

--- bee-transaction/src/serde.rs
+++ bee-transaction/src/serde.rs
@@ -1,37 +1,47 @@
+// Copyright 2020 IOTA Stiftung
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
 use crate::bundled::{
-  Address, BundledTransactionField, Nonce, Payload, Tag, ADDRESS_TRIT_LEN, NONCE_TRIT_LEN, PAYLOAD_TRIT_LEN,
-  TAG_TRIT_LEN,
+    Address, BundledTransactionField, Nonce, Payload, Tag, ADDRESS_TRIT_LEN, NONCE_TRIT_LEN, PAYLOAD_TRIT_LEN,
+    TAG_TRIT_LEN,
 };
 use bee_ternary::{T1B1Buf, TritBuf, Trits, T1B1};
-use serde::ser::Error as SerError;
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{ser::Error as SerError, Deserialize, Deserializer, Serialize, Serializer};
 
 macro_rules! impl_transaction_field_serde {
-  ($field_name:ident, $trit_len:expr) => {
-    impl Serialize for $field_name {
-      fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-      where
-        S: Serializer,
-      {
-        TritBuf::serialize(
-          &Trits::<T1B1>::try_from_raw(self.to_inner().as_i8_slice(), $trit_len)
-            .map_err(|_| SerError::custom("failed to get Trits from Hash"))?
-            .to_buf::<T1B1Buf>(),
-          serializer,
-        )
-      }
-    }
+    ($field_name:ident, $trit_len:expr) => {
+        impl Serialize for $field_name {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: Serializer,
+            {
+                TritBuf::serialize(
+                    &Trits::<T1B1>::try_from_raw(self.to_inner().as_i8_slice(), $trit_len)
+                        .map_err(|_| SerError::custom("failed to get Trits from Hash"))?
+                        .to_buf::<T1B1Buf>(),
+                    serializer,
+                )
+            }
+        }
 
-    impl<'de> Deserialize<'de> for $field_name {
-      fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-      where
-        D: Deserializer<'de>,
-      {
-        TritBuf::deserialize(deserializer)
-          .map(|buf: TritBuf<T1B1Buf>| $field_name::from_inner_unchecked(buf.as_slice().encode()))
-      }
-    }
-  };
+        impl<'de> Deserialize<'de> for $field_name {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                TritBuf::deserialize(deserializer)
+                    .map(|buf: TritBuf<T1B1Buf>| $field_name::from_inner_unchecked(buf.as_slice().encode()))
+            }
+        }
+    };
 }
 
 impl_transaction_field_serde!(Payload, PAYLOAD_TRIT_LEN);
@@ -41,41 +51,41 @@ impl_transaction_field_serde!(Nonce, NONCE_TRIT_LEN);
 
 #[cfg(test)]
 mod tests {
-  use crate::bundled::{Address, Nonce, Payload, Tag};
+    use crate::bundled::{Address, Nonce, Payload, Tag};
 
-  #[test]
-  fn address_serde_check() {
-    let address = Address::zeros();
-    let serialized = serde_json::to_string(&address).expect("failed to serialize address");
-    let deserialized: Address = serde_json::from_str(&serialized).expect("failed to deserialize address");
+    #[test]
+    fn address_serde_check() {
+        let address = Address::zeros();
+        let serialized = serde_json::to_string(&address).expect("failed to serialize address");
+        let deserialized: Address = serde_json::from_str(&serialized).expect("failed to deserialize address");
 
-    assert!(address == deserialized);
-  }
+        assert!(address == deserialized);
+    }
 
-  #[test]
-  fn nonce_serde_check() {
-    let nonce = Nonce::zeros();
-    let serialized = serde_json::to_string(&nonce).expect("failed to serialize nonce");
-    let deserialized: Nonce = serde_json::from_str(&serialized).expect("failed to deserialize nonce");
+    #[test]
+    fn nonce_serde_check() {
+        let nonce = Nonce::zeros();
+        let serialized = serde_json::to_string(&nonce).expect("failed to serialize nonce");
+        let deserialized: Nonce = serde_json::from_str(&serialized).expect("failed to deserialize nonce");
 
-    assert!(nonce == deserialized);
-  }
+        assert!(nonce == deserialized);
+    }
 
-  #[test]
-  fn payload_serde_check() {
-    let payload = Payload::zeros();
-    let serialized = serde_json::to_string(&payload).expect("failed to serialize payload");
-    let deserialized: Payload = serde_json::from_str(&serialized).expect("failed to deserialize payload");
+    #[test]
+    fn payload_serde_check() {
+        let payload = Payload::zeros();
+        let serialized = serde_json::to_string(&payload).expect("failed to serialize payload");
+        let deserialized: Payload = serde_json::from_str(&serialized).expect("failed to deserialize payload");
 
-    assert!(payload == deserialized);
-  }
+        assert!(payload == deserialized);
+    }
 
-  #[test]
-  fn tag_serde_check() {
-    let tag = Tag::zeros();
-    let serialized = serde_json::to_string(&tag).expect("failed to serialize tag");
-    let deserialized: Tag = serde_json::from_str(&serialized).expect("failed to deserialize tag");
+    #[test]
+    fn tag_serde_check() {
+        let tag = Tag::zeros();
+        let serialized = serde_json::to_string(&tag).expect("failed to serialize tag");
+        let deserialized: Tag = serde_json::from_str(&serialized).expect("failed to deserialize tag");
 
-    assert!(tag == deserialized);
-  }
+        assert!(tag == deserialized);
+    }
 }
