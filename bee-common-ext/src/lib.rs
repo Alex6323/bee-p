c929bc2153813b6c23d735e6a4ef6af3824c2c75
Thibault Martinez <thibault@iota.org>
2020-07-02 09:43:06 +0200

    common-ext crate

--- bee-common-ext/src/lib.rs
+++ bee-common-ext/src/lib.rs
@@ -9,7 +9,6 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-pub mod logger;
 pub mod shutdown;
 pub mod wait_priority_queue;
 pub mod worker;
