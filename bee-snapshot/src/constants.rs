9a506e4baf5ead76670e76bffa341ef27350f0b6
Thibault Martinez <thibault@iota.org>
2020-07-05 16:28:05 +0200

    Global snapshot parsing

--- bee-snapshot/src/constants.rs
+++ bee-snapshot/src/constants.rs
@@ -9,8 +9,4 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-pub mod config;
-pub mod constants;
-pub mod global;
-pub mod local;
-pub mod state;
+pub const IOTA_SUPPLY: u64 = 2_779_530_283_277_761;
