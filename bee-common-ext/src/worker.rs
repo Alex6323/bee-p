d3f79e02b49250e1b907234adee8b1ce5464fe10
/alex/ <alexander.schmidt@iota.org>
2020-07-07 15:42:27 +0200

    Fix typo

--- bee-common-ext/src/worker.rs
+++ bee-common-ext/src/worker.rs
@@ -20,7 +20,7 @@ pub enum Error {
     #[error("An asynchronous operation failed.")]
     AsynchronousOperationFailed(#[from] std::io::Error),
 
-    /// Occurs, when a message couldn't be sent over an `futures::channel::mpsc` channel.
+    /// Occurs, when a message couldn't be sent over a `futures::channel::mpsc` channel.
     #[error("Sending a message to a task failed.")]
     SendingMessageFailed(#[from] futures::channel::mpsc::SendError),
 }
