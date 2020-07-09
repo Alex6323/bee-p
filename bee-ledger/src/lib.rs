bf6ffc52c9d55cb78dfaf693e1ffba597f917e71
Thibault Martinez <thibault@iota.org>
2020-07-09 11:39:31 +0200

    Add empty confirmation ledger worker

--- bee-ledger/src/lib.rs
+++ bee-ledger/src/lib.rs
@@ -9,11 +9,13 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+mod confirmation;
 mod merkle;
-mod workers;
+mod state;
 
+pub use confirmation::{LedgerConfirmationWorker, LedgerConfirmationWorkerEvent};
 pub(crate) use merkle::Merkle;
-pub use workers::{LedgerStateWorker, LedgerStateWorkerEvent};
+pub use state::{LedgerStateWorker, LedgerStateWorkerEvent};
 
 use bee_common::shutdown::Shutdown;
 use bee_transaction::bundled::Address;
@@ -26,13 +28,24 @@ use std::collections::HashMap;
 // TODO get concrete type
 pub fn init(state: HashMap<Address, u64>, shutdown: &mut Shutdown) -> mpsc::Sender<LedgerStateWorkerEvent> {
     // TODO config
-    let (ledger_worker_tx, ledger_worker_rx) = mpsc::channel(1000);
-    let (ledger_worker_shutdown_tx, ledger_worker_shutdown_rx) = oneshot::channel();
+    let (ledger_state_worker_tx, ledger_state_worker_rx) = mpsc::channel(1000);
+    let (ledger_state_worker_shutdown_tx, ledger_state_worker_shutdown_rx) = oneshot::channel();
 
     shutdown.add_worker_shutdown(
-        ledger_worker_shutdown_tx,
-        spawn(LedgerStateWorker::new(state).run(ledger_worker_rx, ledger_worker_shutdown_rx)),
+        ledger_state_worker_shutdown_tx,
+        spawn(LedgerStateWorker::new(state).run(ledger_state_worker_rx, ledger_state_worker_shutdown_rx)),
     );
 
-    ledger_worker_tx
+    // TODO config
+    let (_, ledger_confirmation_worker_rx) = mpsc::channel(1000);
+    let (ledger_confirmation_worker_shutdown_tx, ledger_confirmation_worker_shutdown_rx) = oneshot::channel();
+
+    shutdown.add_worker_shutdown(
+        ledger_confirmation_worker_shutdown_tx,
+        spawn(
+            LedgerConfirmationWorker::new().run(ledger_confirmation_worker_rx, ledger_confirmation_worker_shutdown_rx),
+        ),
+    );
+
+    ledger_state_worker_tx
 }
