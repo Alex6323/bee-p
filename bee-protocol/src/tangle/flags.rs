bc85128c47423388eb56c55f9a026607cef123bd
Thibault Martinez <thibault@iota.org>
2020-08-17 09:39:23 +0200

    Add conflicting flag

--- bee-protocol/src/tangle/flags.rs
+++ bee-protocol/src/tangle/flags.rs
@@ -19,6 +19,7 @@ bitflags! {
         const REQUESTED = 0b0000_0100;
         const MILESTONE = 0b0000_1000;
         const CONFIRMED = 0b0001_0000;
+        const CONFLICTING = 0b0010_0000;
     }
 }
 
@@ -61,5 +62,13 @@ impl Flags {
 
     pub fn set_confirmed(&mut self) {
         self.insert(Flags::CONFIRMED);
+    }
+
+    pub fn is_conflicting(&self) -> bool {
+        self.contains(Flags::CONFLICTING)
+    }
+
+    pub fn set_conflicting(&mut self) {
+        self.insert(Flags::CONFLICTING);
     }
 }
