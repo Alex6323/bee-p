5538db82e38a13eb24fd03b2a98dcf09a11c3f78
Thibault Martinez <thibault@iota.org>
2020-08-21 14:15:43 +0200

    Remove scripts

--- bee-node/scripts/download_local_snapshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-url="https://dbfiles.iota.org/mainnet/hornet/latest-export.bin"
-data_folder_path=$(git rev-parse --show-toplevel)/bee-node/snapshots/mainnet
-
-mkdir -p $data_folder_path
-wget $url -O $data_folder_path/export.bin -q --show-progress
