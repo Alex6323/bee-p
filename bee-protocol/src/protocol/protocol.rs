4d848e0f6c9a203c44e38e2d6ed7a41dcf7bcf2e
Christian Poveda <christian.poveda@iota.org>
2020-08-21 15:47:11 -0500

    use ShutdownStream for the MilestoneTransactionWorker

--- bee-protocol/src/protocol/protocol.rs
+++ bee-protocol/src/protocol/protocol.rs
@@ -182,7 +182,13 @@ impl Protocol {
 
         shutdown.add_worker_shutdown(
             milestone_requester_worker_shutdown_tx,
-            spawn(MilestoneRequesterWorker::new().run(milestone_requester_worker_shutdown_rx)),
+            spawn(
+                MilestoneRequesterWorker::new(ShutdownStream::new(
+                    milestone_requester_worker_shutdown_rx,
+                    Protocol::get().milestone_requester_worker.incoming(),
+                ))
+                .run(),
+            ),
         );
 
         match Protocol::get().config.coordinator.sponge_type {
