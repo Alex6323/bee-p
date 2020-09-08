94d8254eda1e25d9b4e125dead8e33ded30b314c
Thibault Martinez <thibault@iota.org>
2020-09-08 10:47:42 +0200

    Actually validate bundles

--- bee-protocol/src/worker/bundle_validator.rs
+++ bee-protocol/src/worker/bundle_validator.rs
@@ -9,14 +9,17 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+use crate::tangle::tangle;
+
 use bee_common::{shutdown_stream::ShutdownStream, worker::Error as WorkerError};
 use bee_crypto::ternary::Hash;
+use bee_tangle::helper::load_bundle_builder;
 
 use futures::{
     channel::mpsc,
     stream::{Fuse, StreamExt},
 };
-use log::info;
+use log::{info, warn};
 
 type Receiver = ShutdownStream<Fuse<mpsc::UnboundedReceiver<BundleValidatorWorkerEvent>>>;
 
@@ -31,7 +34,20 @@ impl BundleValidatorWorker {
         Self { receiver }
     }
 
-    fn validate(&self, tail_hash: Hash) {}
+    fn validate(&self, tail_hash: Hash) {
+        match load_bundle_builder(tangle(), &tail_hash) {
+            Some(builder) => {
+                if let Ok(_) = builder.validate() {
+                    tangle().update_metadata(&tail_hash, |metadata| {
+                        metadata.flags.set_valid(true);
+                    })
+                }
+            }
+            None => {
+                warn!("Faild to validate bundle: tail not found.");
+            }
+        }
+    }
 
     pub(crate) async fn run(mut self) -> Result<(), WorkerError> {
         info!("Running.");
