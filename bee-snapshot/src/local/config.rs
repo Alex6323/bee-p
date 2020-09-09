c2098f3d32435f0c1ca69aa71006d40229dce9de
Thibault Martinez <thibault@iota.org>
2020-09-09 13:14:07 +0200

    Local snapshot conf

--- bee-snapshot/src/local/config.rs
+++ bee-snapshot/src/local/config.rs
@@ -13,11 +13,17 @@ use serde::Deserialize;
 
 const DEFAULT_PATH: &str = "./snapshots/mainnet/export.bin";
 const DEFAULT_DOWNLOAD_URLS: Vec<String> = Vec::new();
+const DEFAULT_DEPTH: usize = 50;
+const DEFAULT_INTERVAL_SYNCED: usize = 50;
+const DEFAULT_INTERVAL_UNSYNCED: usize = 1000;
 
 #[derive(Default, Deserialize)]
 pub struct LocalSnapshotConfigBuilder {
     path: Option<String>,
     download_urls: Option<Vec<String>>,
+    depth: Option<usize>,
+    interval_synced: Option<usize>,
+    interval_unsynced: Option<usize>,
 }
 
 impl LocalSnapshotConfigBuilder {
@@ -35,10 +41,28 @@ impl LocalSnapshotConfigBuilder {
         self
     }
 
+    pub fn depth(mut self, depth: usize) -> Self {
+        self.depth.replace(depth);
+        self
+    }
+
+    pub fn interval_synced(mut self, interval_synced: usize) -> Self {
+        self.interval_synced.replace(interval_synced);
+        self
+    }
+
+    pub fn interval_unsynced(mut self, interval_unsynced: usize) -> Self {
+        self.interval_unsynced.replace(interval_unsynced);
+        self
+    }
+
     pub fn finish(self) -> LocalSnapshotConfig {
         LocalSnapshotConfig {
             path: self.path.unwrap_or_else(|| DEFAULT_PATH.to_string()),
             download_urls: self.download_urls.unwrap_or_else(|| DEFAULT_DOWNLOAD_URLS),
+            depth: self.depth.unwrap_or(DEFAULT_DEPTH),
+            interval_synced: self.interval_synced.unwrap_or(DEFAULT_INTERVAL_SYNCED),
+            interval_unsynced: self.interval_unsynced.unwrap_or(DEFAULT_INTERVAL_UNSYNCED),
         }
     }
 }
@@ -47,6 +71,9 @@ impl LocalSnapshotConfigBuilder {
 pub struct LocalSnapshotConfig {
     path: String,
     download_urls: Vec<String>,
+    depth: usize,
+    interval_synced: usize,
+    interval_unsynced: usize,
 }
 
 impl LocalSnapshotConfig {
@@ -60,5 +87,17 @@ impl LocalSnapshotConfig {
 
     pub fn download_urls(&self) -> &Vec<String> {
         &self.download_urls
+    }
+
+    pub fn depth(&self) -> usize {
+        self.depth
+    }
+
+    pub fn interval_synced(&self) -> usize {
+        self.interval_synced
+    }
+
+    pub fn interval_unsynced(&self) -> usize {
+        self.interval_unsynced
     }
 }
