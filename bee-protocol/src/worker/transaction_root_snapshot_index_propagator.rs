238ea5ec0dffff8846c3b77cdcd07a963a75418b
rusfam <samuel.rufinatscha@iota.org>
2020-09-12 19:45:00 +0200

    Remove unnecessary dependency

--- bee-protocol/src/worker/transaction_root_snapshot_index_propagator.rs
+++ bee-protocol/src/worker/transaction_root_snapshot_index_propagator.rs
@@ -10,7 +10,6 @@
 // See the License for the specific language governing permissions and limitations under the License.
 
 use crate::{
-    event::{LatestSolidMilestoneChanged, TransactionSolidified},
     milestone::Milestone,
     protocol::Protocol,
     tangle::tangle,
