2e1079884dd493388bf5d78d2dbb316ddee77d34
rusfam <samuel.rufinatscha@iota.org>
2020-10-07 16:14:52 +0200

    match instead of if/else

--- bee-protocol/src/tangle/mod.rs
+++ bee-protocol/src/tangle/mod.rs
@@ -199,23 +199,25 @@ impl MsTangle {
     }
 
     pub fn otrsi(&self, hash: &Hash) -> Option<MilestoneIndex> {
-        if let Some(sep) = self.solid_entry_points.get(hash) {
-            Some(*sep.value())
-        } else {
-            match self.get_metadata(hash) {
-                Some(metadata) => metadata.otrsi(),
-                None => None,
+        match self.solid_entry_points.get(hash) {
+            Some(sep) => Some(*sep.value()),
+            None => {
+                match self.get_metadata(hash) {
+                    Some(metadata) => metadata.otrsi(),
+                    None => None,
+                }
             }
         }
     }
 
     pub fn ytrsi(&self, hash: &Hash) -> Option<MilestoneIndex> {
-        if let Some(sep) = self.solid_entry_points.get(hash) {
-            Some(*sep.value())
-        } else {
-            match self.get_metadata(hash) {
-                Some(metadata) => metadata.ytrsi(),
-                None => None,
+        match self.solid_entry_points.get(hash) {
+            Some(sep) => Some(*sep.value()),
+            None => {
+                match self.get_metadata(hash) {
+                    Some(metadata) => metadata.ytrsi(),
+                    None => None,
+                }
             }
         }
     }
