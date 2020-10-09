d224412a671e8b759e7c95050c1168eecb3ed03b
Christian Poveda <christian.poveda@iota.org>
2020-10-09 19:35:15 +0200

    fix broken test

--- bee-transaction/tests/utxo_input.rs
+++ bee-transaction/tests/utxo_input.rs
@@ -9,15 +9,15 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-use bee_transaction::prelude::{MessageId, UTXOInput};
+use bee_transaction::prelude::{TransactionId, UTXOInput};
 
 #[test]
 fn to_string() {
     let mut bytes = [0; 32];
     let vec = hex::decode("52fdfc072182654f163f5f0f9a621d729566c74d10037c4d7bbb0407d1e2c649").unwrap();
     bytes.copy_from_slice(&vec);
-    let message_id = MessageId::new(bytes);
-    let utxo_input = UTXOInput::new(message_id, 1).unwrap();
+    let transaction_id = TransactionId::new(bytes);
+    let utxo_input = UTXOInput::new(transaction_id, 1).unwrap();
     assert_eq!(
         utxo_input.to_string(),
         "52fdfc072182654f163f5f0f9a621d729566c74d10037c4d7bbb0407d1e2c6490100"
