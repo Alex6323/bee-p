acab7a1dd0ea60285ef7a7718f8d181fe952a03a
Ngo Iok Ui <wusyong9104@gmail.com>
2020-10-13 18:12:36 +0800

    Update address display to hex string

--- bee-message/src/payload/transaction/output/address/ed25519.rs
+++ bee-message/src/payload/transaction/output/address/ed25519.rs
@@ -49,7 +49,7 @@ impl Ed25519Address {
 
 impl core::fmt::Display for Ed25519Address {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        write!(f, "{}", self.to_bech32())
+        write!(f, "{}", hex::encode(self.0))
     }
 }
 
