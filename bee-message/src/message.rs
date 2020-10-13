222bcf4badf0e985b8604981795c1e58ea53bb2d
Thibault Martinez <thibault@iota.org>
2020-10-13 09:53:02 +0200

    Udeps, fmt and cargo update

--- bee-message/src/message.rs
+++ bee-message/src/message.rs
@@ -9,10 +9,7 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-use crate::{
-    Vertex,
-    {payload::Payload, Error, MessageId},
-};
+use crate::{payload::Payload, Error, MessageId, Vertex};
 
 use bee_common_ext::packable::{Error as PackableError, Packable, Read, Write};
 
