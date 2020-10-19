aeab33be6f880d5b79bc446899183c819f438bb2
Thibault Martinez <thibault@iota.org>
2020-10-19 15:24:07 +0200

    Move event tests

--- bee-common-ext/src/event.rs
+++ bee-common-ext/src/event.rs
@@ -38,37 +38,3 @@ impl<'a> Bus<'a> {
             }));
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    // use test::{Bencher, black_box};
-
-    struct Foo;
-
-    #[test]
-    fn basic() {
-        let bus = Bus::default();
-
-        bus.add_listener(|_: &Foo| println!("Received a foo!"));
-
-        bus.dispatch(Foo);
-    }
-
-    #[test]
-    fn send_sync() {
-        fn helper<T: Send + Sync>() {}
-        helper::<Bus<'static>>();
-    }
-
-    // #[bench]
-    // fn bench_add_two(b: &mut Bencher) {
-    //     let bus = Bus::default();
-
-    //     bus.add_listener(|e: &Foo| { black_box(e); });
-
-    //     b.iter(|| {
-    //         bus.dispatch(Foo);
-    //     });
-    // }
-}
---
+++ bee-common-ext/tests/event.rs
@@ -0,0 +1,40 @@
+// Copyright 2020 IOTA Stiftung
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
+use bee_common_ext::event::Bus;
+
+struct Foo;
+
+#[test]
+fn basic() {
+    let bus = Bus::default();
+
+    bus.add_listener(|_: &Foo| println!("Received a foo!"));
+
+    bus.dispatch(Foo);
+}
+
+#[test]
+fn send_sync() {
+    fn helper<T: Send + Sync>() {}
+    helper::<Bus<'static>>();
+}
+
+// #[bench]
+// fn bench_add_two(b: &mut Bencher) {
+//     let bus = Bus::default();
+
+//     bus.add_listener(|e: &Foo| { black_box(e); });
+
+//     b.iter(|| {
+//         bus.dispatch(Foo);
+//     });
+// }
