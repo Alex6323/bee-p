09ad92c6365dcdb6d75103e9a3f4f46515f18a4e
Thibault Martinez <thibault@iota.org>
2020-10-20 14:42:40 +0200

    Remove unused clippy allow

--- bee-message/src/lib.rs
+++ bee-message/src/lib.rs
@@ -10,7 +10,6 @@
 // See the License for the specific language governing permissions and limitations under the License.
 
 #![no_std]
-#![allow(clippy::module_inception)]
 
 #[macro_use]
 extern crate alloc;
