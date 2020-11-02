2db1354e134037dd313edec9d1364fd59f7722f3
Thibault Martinez <thibault@iota.org>
2020-11-02 10:16:19 +0100

    Sort key ranges by start index

--- bee-protocol/src/milestone/key_manager.rs
+++ bee-protocol/src/milestone/key_manager.rs
@@ -20,7 +20,9 @@ pub(crate) struct KeyManager {
 }
 
 impl KeyManager {
-    pub(crate) fn new(min_threshold: usize, key_ranges: Box<[KeyRange]>) -> Self {
+    pub(crate) fn new(min_threshold: usize, mut key_ranges: Box<[KeyRange]>) -> Self {
+        key_ranges.sort();
+
         Self {
             min_threshold,
             key_ranges,
@@ -46,4 +48,32 @@ impl KeyManager {
 
         public_keys
     }
+}
+
+#[test]
+fn key_manager_sort() {
+    let krs = vec![
+        KeyRange::new("kr1".to_string(), 42.into(), 1000.into()),
+        KeyRange::new("kr2".to_string(), 21.into(), 1000.into()),
+        KeyRange::new("kr3".to_string(), 84.into(), 1000.into()),
+        KeyRange::new("kr4".to_string(), 0.into(), 1000.into()),
+    ];
+
+    let km = KeyManager::new(0, krs.into_boxed_slice());
+
+    assert_eq!(km.key_ranges[0].public_key(), "kr4");
+    assert_eq!(km.key_ranges[0].start(), 0.into());
+    assert_eq!(km.key_ranges[0].end(), 1000.into());
+
+    assert_eq!(km.key_ranges[1].public_key(), "kr2");
+    assert_eq!(km.key_ranges[1].start(), 21.into());
+    assert_eq!(km.key_ranges[1].end(), 1000.into());
+
+    assert_eq!(km.key_ranges[2].public_key(), "kr1");
+    assert_eq!(km.key_ranges[2].start(), 42.into());
+    assert_eq!(km.key_ranges[2].end(), 1000.into());
+
+    assert_eq!(km.key_ranges[3].public_key(), "kr3");
+    assert_eq!(km.key_ranges[3].start(), 84.into());
+    assert_eq!(km.key_ranges[3].end(), 1000.into());
 }
