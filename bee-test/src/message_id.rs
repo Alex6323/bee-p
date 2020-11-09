546843fcd78d0f7bca7563d9bdaa6c06c430525c
Thibault Martinez <thibault@iota.org>
2020-11-09 19:42:14 +0100

    Random message and message_id generation for tests

--- bee-test/src/message_id.rs
+++ bee-test/src/message_id.rs
@@ -9,6 +9,10 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-pub mod message;
-pub mod message_id;
-pub mod milestone;
+use bee_message::{MessageId, MESSAGE_ID_LENGTH};
+
+use rand::Rng;
+
+pub fn random_message_id() -> MessageId {
+    MessageId::new(rand::thread_rng().gen::<[u8; MESSAGE_ID_LENGTH]>())
+}
