76577acbc4aad764d5986893e8dbda3cdc4cf62a
Thibault Martinez <thibault@iota.org>
2020-11-10 11:39:01 +0100

    random_string utility

--- bee-test/src/string.rs
+++ bee-test/src/string.rs
@@ -9,8 +9,9 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-pub mod bytes;
-pub mod message;
-pub mod message_id;
-pub mod milestone;
-pub mod string;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+
+pub fn random_string(len: usize) -> String {
+    thread_rng().sample_iter(&Alphanumeric).take(len).collect()
+}
