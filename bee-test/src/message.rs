d1c003c491482adf279a43e111c2c272a995016c
Thibault Martinez <thibault@iota.org>
2020-11-10 12:00:17 +0100

    Improve random_message

--- bee-test/src/message.rs
+++ bee-test/src/message.rs
@@ -9,9 +9,15 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-use crate::bytes::random_bytes_32;
+use crate::{
+    bytes::{random_bytes, random_bytes_32},
+    string::random_string,
+};
 
-use bee_message::{Message, MessageId};
+use bee_message::{
+    payload::{indexation::Indexation, Payload},
+    Message, MessageId,
+};
 
 use rand::Rng;
 
@@ -19,13 +25,25 @@ pub fn random_message_id() -> MessageId {
     MessageId::new(random_bytes_32())
 }
 
-pub fn random_message() -> Message {
-    let mut rng = rand::thread_rng();
+pub fn random_indexation() -> Payload {
+    Indexation::new(random_string(32), &random_bytes(64)).into()
+}
 
+pub fn random_payload() -> Payload {
+    // TODO complete with other types
+    random_indexation()
+}
+
+pub fn random_message_with_parents(parent1: MessageId, parent2: MessageId) -> Message {
     Message::builder()
-        .with_parent1(random_message_id())
-        .with_parent2(random_message_id())
-        .with_nonce(rng.gen::<u64>())
+        .with_parent1(parent1)
+        .with_parent2(parent2)
+        .with_payload(random_payload())
+        .with_nonce(rand::thread_rng().gen::<u64>())
         .finish()
         .unwrap()
+}
+
+pub fn random_message() -> Message {
+    random_message_with_parents(random_message_id(), random_message_id())
 }
