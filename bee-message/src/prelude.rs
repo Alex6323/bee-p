8f6ea594f8b0a114ee5f0fb52e4bd6d63d572a65
Thibault Martinez <thibault@iota.org>
2020-11-11 10:21:56 +0100

    Fix Milestone packed_len

--- bee-message/src/prelude.rs
+++ bee-message/src/prelude.rs
@@ -12,7 +12,9 @@
 pub use crate::{
     payload::{
         indexation::{HashedIndex, Indexation, HASHED_INDEX_SIZE},
-        milestone::Milestone,
+        milestone::{
+            Milestone, MILESTONE_MERKLE_PROOF_LENGTH, MILESTONE_PUBLIC_KEY_LENGTH, MILESTONE_SIGNATURE_LENGTH,
+        },
         transaction::{
             Address, Ed25519Address, Ed25519Signature, Input, Output, OutputId, ReferenceUnlock,
             SignatureLockedSingleOutput, SignatureUnlock, Transaction, TransactionBuilder, TransactionEssence,
