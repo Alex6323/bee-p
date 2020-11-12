dea0335e56c21ddeddaf9b29f8dee770b5a70a39
Thibault Martinez <thibault@iota.org>
2020-11-12 11:28:43 +0100

    Fmt

--- bee-test/src/string.rs
+++ bee-test/src/string.rs
@@ -9,8 +9,7 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
 
 pub fn random_string(len: usize) -> String {
     thread_rng().sample_iter(&Alphanumeric).take(len).collect()
