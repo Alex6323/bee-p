1e5f7ccf07a8cf5bbd7bb1d39fcae4f738254d2c
Thibault Martinez <thibault@iota.org>
2020-11-13 13:08:49 +0100

    bee-test random_integer function

--- bee-test/src/rand/integer.rs
+++ bee-test/src/rand/integer.rs
@@ -9,7 +9,14 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-pub mod bytes;
-pub mod message;
-pub mod string;
-pub mod integer;
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+
+pub fn random_integer<T>() -> T
+where
+    Standard: Distribution<T>,
+{
+    rand::thread_rng().gen::<T>()
+}
