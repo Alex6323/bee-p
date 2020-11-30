14b30e643b425177522b168d67084723e430f418
rusfam <samuel.rufinatscha@iota.org>
2020-11-30 21:22:20 +0100

    update deps, format

--- bee-api/src/storage.rs
+++ bee-api/src/storage.rs
@@ -1,3 +1,6 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use bee_ledger::{output::Output, spent::Spent};
 use bee_message::{
     payload::transaction::{Ed25519Address, OutputId},
