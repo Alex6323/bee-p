0e577e40cf52dc10fd38dc504540ca74dbdf7d78
Thibault Martinez <thibault@iota.org>
2020-12-01 09:49:02 +0100

    Move bee-storage-rocksdb to bee

--- bee-storage/bee-storage-rocksdb/src/access/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-pub mod batch;
-pub mod delete;
-pub mod exist;
-pub mod fetch;
-pub mod insert;
-pub mod stream;
--- bee-storage/bee-storage-rocksdb/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-pub mod access;
-pub mod compaction;
-pub mod compression;
-pub mod config;
-pub mod error;
-pub mod storage;
